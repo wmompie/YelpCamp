d17cc24b9c096dfaa1b7aac22240f25a326dd63a
wmompie <wmompie@gmail.com>
2019-02-22 10:38:35 -0500

    styling for campgrounds, form and installed navbar

--- app.js
+++ app.js
@@ -16,6 +16,30 @@ let campgrounds = [
   {
     name: `Mountain Goat's Rest`,
     image: `https://images.unsplash.com/photo-1440262206549-8fe2c3b8bf8f?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=500&q=60`
+  },
+  {
+    name: `Salmon Creek`,
+    image: `https://images.unsplash.com/photo-1515408320194-59643816c5b2?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=500&q=60`
+  },
+  {
+    name: `Granite Hill`,
+    image: `https://images.unsplash.com/photo-1525209149972-1d3faa797c3c?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=500&q=60`
+  },
+  {
+    name: `Mountain Goat's Rest`,
+    image: `https://images.unsplash.com/photo-1440262206549-8fe2c3b8bf8f?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=500&q=60`
+  },
+  {
+    name: `Salmon Creek`,
+    image: `https://images.unsplash.com/photo-1515408320194-59643816c5b2?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=500&q=60`
+  },
+  {
+    name: `Granite Hill`,
+    image: `https://images.unsplash.com/photo-1525209149972-1d3faa797c3c?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=500&q=60`
+  },
+  {
+    name: `Mountain Goat's Rest`,
+    image: `https://images.unsplash.com/photo-1440262206549-8fe2c3b8bf8f?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=500&q=60`
   }
 ];
 
