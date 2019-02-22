a73dc7b66d3ad82796109ba24193e20c53c0e1c0
wmompie <wmompie@gmail.com>
2019-02-21 22:10:59 -0500

    Initial Routes

--- app.js
+++ app.js
@@ -1,1 +1,32 @@
-app;
+const express = require('express');
+const app = express();
+app.set('view engine', 'ejs');
+
+app.get('/', (req, res) => {
+  res.render('landing');
+});
+
+app.get('/campgrounds', (req, res) => {
+  let campgrounds = [
+    {
+      name: 'Salmon Creek',
+      image:
+        'https://images.unsplash.com/photo-1515408320194-59643816c5b2?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=500&q=60'
+    },
+    {
+      name: 'Granite Hill',
+      image:
+        'https://images.unsplash.com/photo-1525209149972-1d3faa797c3c?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=500&q=60'
+    },
+    {
+      name: "Mountain Goat's Rest",
+      image:
+        'https://images.unsplash.com/photo-1440262206549-8fe2c3b8bf8f?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=500&q=60'
+    }
+  ];
+  res.render('campgrounds', { campgrounds: campgrounds });
+});
+
+app.listen(3000, () => {
+  console.log('The YelpCamp Server Has Started!');
+});
