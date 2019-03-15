6f532362bd4edcf2e0f7e96cf43c957774878553
wmompie <wmompie@gmail.com>
2019-03-15 00:38:17 -0400

    cleaning up app.js

--- app.js
+++ app.js
@@ -6,9 +6,7 @@ const express = require('express'),
   LocalStrategy = require('passport-local'),
   app = express();
 
-const Campground = require('./models/campground'),
-  Comment = require('./models/comment'),
-  User = require('./models/user'),
+const User = require('./models/user'),
   seedDB = require('./seeds');
 
 const commentRoutes = require('./routes/comments'),
