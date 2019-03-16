fa23029d1d8411cbae8754dda063d9d56933e590
wmompie <wmompie@gmail.com>
2019-03-16 00:40:14 -0400

    testing config against Heroku and mongoose

--- config/keys_prod.js
+++ config/keys_prod.js
@@ -1,4 +1,4 @@
 module.exports = {
-  mongoURI: process.env.MONGO_URI,
-  secretOrKey: process.env.SECRET_OR_KEY
+  mongoURI: 'mongodb+srv://wmompie:wofw2885@yelpcamp-iz3os.mongodb.net/test?retryWrites=true',
+  secretOrKey: 'THIS IS MY VERY SPECIAL SECRET'
 };
